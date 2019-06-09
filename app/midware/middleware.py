0c656f8cd0f0d384bb824d6cecdc86250617c21e
guiaiy <erdongmuxin@163.com>
2019-06-09 09:34:16 +0800

    no message

--- app/midware/middleware.py
+++ app/midware/middleware.py
@@ -7,7 +7,7 @@ from django.utils.deprecation import MiddlewareMixin
 
 class RequireLoginMiddleware(MiddlewareMixin):
     def process_view(self, request, view_func, *args, **kwargs):
-        print(request.path)
+        # print(request.path)
         if 'islogin' in request.session or re.match('/login', request.path) or re.match('/admin/', request.path):
             return
         else:
